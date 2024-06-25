d8d86c9e0f1d3878e4b79799e628b6df88c1a9a3
Larissa Helfer <larissa.helfer@tekann.com.br>
2024-06-25 18:55:43 -0300

    teste

--- AstroMonitor/src/main/java/br/com/astromonitor/astromonitor/utils/Connection.java
+++ AstroMonitor/src/main/java/br/com/astromonitor/astromonitor/utils/Connection.java
@@ -12,6 +12,6 @@ public class Connection {
     
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/astro_monitor";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "root";
+    //private static final String JDBC_PASSWORD = "root";
     
 }
